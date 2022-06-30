d2de27c38a080f1d72ba44b840c9c45d3b815d74
Your Name <you@example.com>
2022-06-30 22:44:20 +0200

    first commit

---
+++ defaultapplication/scripts/serverStatus.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/home/ibmdemo/m2m-ws-sample/defaultapplication/monolith-mono2micro/DefaultApplication-ear/target/liberty/wlp/bin/server status DefaultApplicationServer
+
---
+++ defaultapplication/scripts/viewServerLog.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat /home/ibmdemo/m2m-ws-sample/defaultapplication/monolith-mono2micro/DefaultApplication-ear/target/liberty/wlp/usr/servers/DefaultApplicationServer/logs/messages.log
